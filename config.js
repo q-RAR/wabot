81d3944dda7af26d487bab85e71b69a262ef45aa
q-RAR <90776792+q-RAR@users.noreply.github.com>
2023-01-10 01:11:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -10,13 +10,13 @@ global.antitags = true
 //global.autoTyping = false //auto tying in gc (true to on, false to off)
 //global.autoRecord = true //auto recording (true to on, false to off)
 
-global.Owner = ['916297175943','919362639362']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.OwnerNumber = ['916297175943','919362639362']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
-global.ownertag = ['916297175943']
-global.BotName = "A17"
-global.packname = "A17 bot"
-global.author = "By: Kai"
-global.OwnerName = "Kai"
+global.Owner = ['6282199399855','6281299939855']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.OwnerNumber = ['6282199399855','6281299929855']  //If you want singal number so global.Owner = ['916297175943'] Change into your number
+global.ownertag = ['6281299939855']
+global.BotName = "k-RAR"
+global.packname = "k-RAR bot"
+global.author = "By: k-RAR"
+global.OwnerName = "k-RAR"
 global.BotSourceCode = "https://github.com/Kai0071/A17"
 global.SupportGroupLink = "https://chat.whatsapp.com/EsNqFrGKxGXFrdwgRaQl9q"
 global.sessionName = "session"
@@ -26,7 +26,7 @@ global.sessionName = "session"
 global.prefa = ['-'] 
 
 
-global.location = "West Bengal, India"
+global.location = "Makassar, Indonesia"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://media.tenor.com/Jdu0Ov8X2sIAAAAC/A17-Bot.mp4' }
